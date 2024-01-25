eaa0a77ebbe82455253766cb905b329635703d7a
SandraVLP <aleksanvp@yandex.ru>
2024-01-25 11:41:18 +0300

    примерный макет

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -4,9 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+
+      <div className="container">
+        <h1 className="title">TODO List</h1>
+        <form className="todo-form">
+          <input class="todo-input" type="text"  />
+          <button class="todo-btn" type="submit" >Submit</button>
+        </form>
+        <div class="todo-list"></div>
+<p className="task"> Something</p>
+      </div>
+
     </div>
   );
 }
