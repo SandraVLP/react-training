6a1c7d446f824d6db595a53cf19fe3e9afac5867
SandraVLP <aleksanvp@yandex.ru>
2024-01-25 12:35:15 +0300

    component rodoitem

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,43 +1,47 @@
-
-import './App.css';
-import {useState} from 'react'
+import "./App.css";
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { TodoItem } from "./components/TodoItem";
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [todoItems, setTodoItem] = useState([]);
- 
+
   const handleChange = (event) => {
     setInputValue(event.target.value);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     addTodo(inputValue);
-    setInputValue('');
-  }
+    setInputValue("");
+  };
 
   const addTodo = (todo) => {
-    setTodoItem([
-      ...todoItems,
-      { id: uuidv4(), task: todo},
-    ])
-    console.log(todoItems)
-  }
+    setTodoItem([...todoItems, { id: uuidv4(), task: todo }]);
+    console.log(todoItems);
+  };
 
   return (
     <div className="App">
-
       <div className="container">
         <h1 className="title">TODO List</h1>
         <form className="todo-form" onSubmit={handleSubmit}>
-          <input className="todo-input" type="text"  value={inputValue} onChange={() => handleChange()}/>
-          <button className="todo-btn" type="submit" >Submit</button>
+          <input
+            className="todo-input"
+            type="text"
+            value={inputValue}
+            onChange={() => handleChange()}
+          />
+          <button className="todo-btn" type="submit">
+            Submit
+          </button>
         </form>
         <div className="todo-list"></div>
-<p className="task"> Something</p>
+        {todoItems.map((todo, index) => (
+          <TodoItem key={index} task={todo} />
+        ))}
       </div>
-
     </div>
   );
 }
---
+++ todo-list/src/components/TodoItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TodoItem = ({task,}) => {
+    return (
+
+          <p className="task">{task.task}</p>
+
+    )
+  }
