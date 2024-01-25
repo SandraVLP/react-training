e079c2ced78a90725a24384cd7992e4e80cf8769
SandraVLP <aleksanvp@yandex.ru>
2024-01-25 12:41:53 +0300

    убрать данные компонента из апп

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,20 +2,11 @@ import "./App.css";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { TodoItem } from "./components/TodoItem";
+import { TodoForm } from "./components/TodoForm";
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
   const [todoItems, setTodoItem] = useState([]);
 
-  const handleChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addTodo(inputValue);
-    setInputValue("");
-  };
 
   const addTodo = (todo) => {
     setTodoItem([...todoItems, { id: uuidv4(), task: todo }]);
@@ -26,21 +17,11 @@ function App() {
     <div className="App">
       <div className="container">
         <h1 className="title">TODO List</h1>
-        <form className="todo-form" onSubmit={handleSubmit}>
-          <input
-            className="todo-input"
-            type="text"
-            value={inputValue}
-            onChange={() => handleChange()}
-          />
-          <button className="todo-btn" type="submit">
-            Submit
-          </button>
-        </form>
+        <TodoForm addTodo={addTodo} />
         <div className="todo-list"></div>
-        {todoItems.map((todo, index) => (
+        {/* {todoItems.map((todo, index) => (
           <TodoItem key={index} task={todo} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
