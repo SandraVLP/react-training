f02e915f3a378477c55c70792c754eda0e68e62c
SandraVLP <aleksanvp@yandex.ru>
2024-01-25 12:50:57 +0300

    иконки удалить-редактировать

--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const TodoItem = ({task}) => {
-    return (
-
-          <p className="task">{task.task}</p>
-
-    )
-  }
+export const TodoItem = ({ task }) => {
+  return ( <div>
+  <p className="task">{task.task}</p>
+  <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
+  <FontAwesomeIcon className="delete-icon" icon={faTrash} />
+  </div>
+  )
+};
