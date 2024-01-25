45be631121fa4c0dbed680bfb78367126c2b72c0
SandraVLP <aleksanvp@yandex.ru>
2024-01-25 13:02:16 +0300

    delete Item

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -7,6 +7,8 @@ import { TodoForm } from "./components/TodoForm";
 function App() {
   const [todoItems, setTodoItem] = useState([]);
 
+  const deleteTodoItem = (id) => setTodoItem(todoItems.filter((todo) => todo.id !== id));
+
 
   const addTodo = (todo) => {
     setTodoItem([...todoItems, { id: uuidv4(), task: todo }]);
@@ -19,7 +21,7 @@ function App() {
         <h1 className="title">TODO List</h1>
         <TodoForm addTodo={addTodo} />
         {todoItems.map((todo, index) => (
-          <TodoItem key={index} task={todo} />
+          <TodoItem key={index} task={todo} deleteTodoItem={deleteTodoItem}/>
         ))}
       </div>
     </div>
--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -3,11 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const TodoItem = ({ task }) => {
+export const TodoItem = ({ task, deleteTodoItem }) => {
   return ( <div className="todo-container">
   <p className="task">{task.task}</p>
   <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
-  <FontAwesomeIcon className="delete-icon" icon={faTrash} />
+  <FontAwesomeIcon className="delete-icon" icon={faTrash}  onClick={() => deleteTodoItem(task.id)}/>
   </div>
   )
 };
