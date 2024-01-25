7a5a0c364bb1ae17659f632bfed10f095464331d
SandraVLP <aleksanvp@yandex.ru>
2024-01-25 11:56:48 +0300

    usestate,handleChange

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,17 +1,25 @@
 
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+ 
+  /* The handleChange() function to set a new state for input */
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  }
+
   return (
     <div className="App">
 
       <div className="container">
         <h1 className="title">TODO List</h1>
         <form className="todo-form">
-          <input class="todo-input" type="text"  />
-          <button class="todo-btn" type="submit" >Submit</button>
+          <input className="todo-input" type="text"  value={inputValue} onChange={handleChange}/>
+          <button className="todo-btn" type="submit" >Submit</button>
         </form>
-        <div class="todo-list"></div>
+        <div className="todo-list"></div>
 <p className="task"> Something</p>
       </div>
 
